ab0ace54e870596317858ca5aefd7a2c741f54bf
Eliseu <153873892+lzeunfa@users.noreply.github.com>
2025-04-24 21:29:10 -0300

    removi abas desnecessárias para a barbearia, adicionei a aba de fila de espera junto com o tempo de espera e quantos na fila

--- ClubDuRafa.js
+++ ClubDuRafa.js
@@ -6,27 +6,6 @@ servicos.style.display="none";
 let setaSer = document.getElementById("seta-ser");
 setaSer.src="img/seta-para-baixo-branca.png";
 
-//
-
-//var que recebe a div da loc
-let loc = document.getElementById("mostrar-loc");
-loc.style.display ="none";
-
-//var que recebe a img de seta da loc
-let setaLoc = document.getElementById("seta-loc");
-setaLoc.src = "img/seta-para-baixo-branca.png";
-
-//
-
-//var que recebe a div com infos do funcionamento
-let funcionamento = document.getElementById("funcionamento");
-funcionamento.style.display="none";
-
-//var que recebe a imagem de seta do conteiner de funcionamento
-let setaHorario = document.getElementById("seta-horario")
-
-//
-
 //funcao para mostrar a div servicos onclick
 function mostrarServicos(){
     if(servicos.style.display=="none"){
@@ -35,28 +14,5 @@ function mostrarServicos(){
     }else{
         servicos.style.display="none";
         setaSer.src="img/seta-para-baixo-branca.png";
-    }
-}
-
-
-//funcao para mostrar o iframe da localização
-function mostrarLoc(){
-    if(loc.style.display=="none"){
-        loc.style.display="block";
-        setaLoc.src="img/seta-para-cima-branca.png";
-    }else{
-        loc.style.display="none";
-        setaLoc.src="img/seta-para-baixo-branca.png";
-    }
-}
-
-//função para mostrar o funcionamento e horarios
-function horario(){
-    if(funcionamento.style.display=="none"){
-        funcionamento.style.display="block";
-        setaHorario.src="img/seta-para-cima-branca.png";
-    }else{
-        funcionamento.style.display="none";
-        setaHorario.src="img/seta-para-baixo-branca.png";
     }
 }
